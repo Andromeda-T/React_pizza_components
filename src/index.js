984c9213c1dbf177ff683d9d260aeefa64622d45
Radin T <radin.khodadadi0211@gmail.com>
2025-04-09 16:59:01 +0330

    add props function for the footer and make conetion beetwen pizza datas and the pizza function  and use && twice

--- src/index.js
+++ src/index.js
@@ -7,42 +7,42 @@ const pizzaData = [
         name: "Focaccia",
         ingredients: "Bread with italian olive oil and rosemary",
         price: 6,
-        photoName: "pizzas/focaccia.jpg",
+        photoName: "images/focaccia.jpg",
         soldOut: false,
     },
     {
         name: "Pizza Margherita",
         ingredients: "Tomato and mozarella",
         price: 10,
-        photoName: "pizzas/margherita.jpg",
+        photoName: "images/margherita.jpg",
         soldOut: false,
     },
     {
         name: "Pizza Spinaci",
         ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
         price: 12,
-        photoName: "pizzas/spinaci.jpg",
+        photoName: "images/spinaci.jpg",
         soldOut: false,
     },
     {
         name: "Pizza Funghi",
         ingredients: "Tomato, mozarella, mushrooms, and onion",
         price: 12,
-        photoName: "pizzas/funghi.jpg",
+        photoName: "images/funghi.jpg",
         soldOut: false,
     },
     {
         name: "Pizza Salamino",
         ingredients: "Tomato, mozarella, and pepperoni",
         price: 15,
-        photoName: "pizzas/salamino.jpg",
+        photoName: "images/salamino.jpg",
         soldOut: true,
     },
     {
         name: "Pizza Prosciutto",
         ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
         price: 18,
-        photoName: "pizzas/prosciutto.jpg",
+        photoName: "images/prosciutto.jpg",
         soldOut: false,
     },
 ];
@@ -69,39 +69,48 @@ function Header() {
 }
 
 function Menu() {
+    const pizza = pizzaData
+    const numPizzas = pizza.length
     return (
         <main className="menu">
             <h2>Our menu</h2>
-            <Pizza
-                name="Pizza spinaci"
-                ingredients="Bread with italian olive oil and rosemary"
-                photoName="images/spinaci.jpg"
-                price={10} />
-            <Pizza
-                name="Pizza Funghi"
-                ingredients="Bread with italian olive oil and rosemary"
-                photoName="images/funghi.jpg"
-                price= {12} />
+            {numPizzas > 0  &&
+                <div className="pizzas">
+                    {
+                        pizza.map(pizza =>
+                            <Pizza pizzaObj={pizza} key={pizza.name} />)
+                    }
+                </div>}
         </main>
     )
 }
+
 function Pizza(prop) {
     return (
         <div className="pizza">
-            <img src={prop.photoName} alt={prop.name}></img>
+            <img src={prop.pizzaObj.photoName} alt={prop.pizzaObj.name}></img>
             <div className="">
-                <h3>{prop.name}</h3>
-                <p>{prop.ingredients}</p>
-                <span>Pice : {prop.price}$</span>
+                <h3>{prop.pizzaObj.name}</h3>
+                <p>{prop.pizzaObj.ingredients}</p>
+                <span>Pice : {prop.pizzaObj.price}$</span>
             </div>
         </div>
     )
 }
 
 function Footer() {
+    const hour = new Date().getHours()
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour
     return (
         <footer className="footer">
-            {new Date().toLocaleTimeString()} we we`re currently closed
+            {isOpen && (
+                <div className="order">
+                    <p>We`re open until {closeHour}:00. come to visit us or order online !</p>
+                    <button className="btn">Order</button>
+                </div>
+            )}
         </footer>
     )
 }
